9b56adff8165d0e148fde91337ee1b0ca4cd9874
Org42. <roy.prins@org42.net>
2026-05-08 13:58:59 +0200

    fix: remove unused import, add default UUID init to AttachedFile

--- Chat42/Sources/Models/AttachedFile.swift
+++ Chat42/Sources/Models/AttachedFile.swift
@@ -1,8 +1,9 @@
 import Foundation
-import UniformTypeIdentifiers
 
 enum AttachmentType: String, Codable, Hashable {
-  case text, image, pdf
+  case text = "text"
+  case image = "image"
+  case pdf = "pdf"
 
   var systemImage: String {
     switch self {
@@ -20,6 +21,15 @@ struct AttachedFile: Identifiable {
   let type: AttachmentType
   let data: Data
   let mimeType: String  // e.g. "image/jpeg"
+
+  init(id: UUID = UUID(), url: URL, name: String, type: AttachmentType, data: Data, mimeType: String) {
+    self.id = id
+    self.url = url
+    self.name = name
+    self.type = type
+    self.data = data
+    self.mimeType = mimeType
+  }
 }
 
 struct MessageAttachment: Codable, Identifiable, Hashable {
